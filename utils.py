5e779e0de3a55063e87bcdcb423eb9830a3f03fd
Sudarshan <75570463+bheemisme@users.noreply.github.com>
2024-08-31 09:10:31 +0530

    Update utils.py
    added download raw file

--- utils.py
+++ utils.py
@@ -7,3 +7,14 @@ def extract_zip_file(zip_file_path: str, extract_to_path: str):
         zip_ref.extractall(extract_to_path)
     print(f"Extracted all files to {extract_to_path}")
 
+def download_raw_file(repository: str, file_name: str, vc_id: str, branch: str):
+    url = f"https://raw.githubusercontent.com/{vc_id}/{repository}/{branch}/{file_name}"
+
+    # download file from url
+    import requests
+
+    response = requests.get(url)
+    with open(file_name, 'wb') as f:
+        f.write(response.content)
+
+    print(f"File {file_name} downloaded successfully")
