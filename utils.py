4a1b915dc4c561084dc94e374a59f7f27301da8f
Sudarshan <75570463+bheemisme@users.noreply.github.com>
2024-08-31 09:07:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,9 @@
-import zipfile
-from pathlib import Path
 
 # extract zip file to a directory
-def extract_zip_file(zip_file_path: Path, extract_to_path: Path):
-  zip_file_path = Path(zip_file_path)
-  extract_to_path = Path('.')
-  with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-      zip_ref.extractall(extract_to_path)
-  print(f"Extracted all files to {extract_to_path}")
+def extract_zip_file(zip_file_path: str, extract_to_path: str):
+    import zipfile
+    
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to_path)
+    print(f"Extracted all files to {extract_to_path}")
 
